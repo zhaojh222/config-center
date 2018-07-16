7b34267c1ec5bb775754a2e67e531661a3a5bed3
vn0nzx5 <zhaojh222@gmaio.com>
2018-07-16 13:08:14 -0700

    init

---
+++ src/main/java/org/ootb/configcenter/ConfigCenterApplication.java
@@ -0,0 +1,14 @@
+package org.ootb.configcenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigCenterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigCenterApplication.class, args);
+    }
+}
